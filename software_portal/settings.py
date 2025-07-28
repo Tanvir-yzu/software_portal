98626049f42fe156e9d00da595b3134506c8569b
Tanvir Islam <2020tanvir1971@gmail.com>
2025-07-29 00:55:05 +0800

    pak

--- software_portal/settings.py
+++ software_portal/settings.py
@@ -30,12 +30,14 @@ ALLOWED_HOSTS = ALLOWED_HOSTS
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'software',
 ]
 
